e6bc89077bf68aab46cf544a8d82846c7a72d611
Edgar Alvarado <edgar.alvarado.16@gmail.com>
2023-03-24 00:42:33 -0400

    modificado masa_corporal

--- Tema3/masa_corporal.py
+++ Tema3/masa_corporal.py
@@ -5,11 +5,13 @@ print("Introduce tu altura en m")
 altura = float(input())
 indiceMasaCorporal = peso // (altura)**2
 
-if(indiceMasaCorporal<= 18.5):
-        print("Bajo de peso: " + str(indiceMasaCorporal)) 
-elif(indiceMasaCorporal > 18.5 and indiceMasaCorporal <= 24.9):
-        print("Normal: " + str(indiceMasaCorporal))
-elif (indiceMasaCorporal > 25 and indiceMasaCorporal <= 29.9):
-        print("Sobrepeso: " + str(indiceMasaCorporal))
-else:
-        print("Obesidad: " + str(indiceMasaCorporal))
+print("Tu índice de masa corporal es: " + str(indiceMasaCorporal))
+
+# if(indiceMasaCorporal<= 18.5):
+#         print("Bajo de peso: " + str(indiceMasaCorporal)) 
+# elif(indiceMasaCorporal > 18.5 and indiceMasaCorporal <= 24.9):
+#         print("Normal: " + str(indiceMasaCorporal))
+# elif (indiceMasaCorporal > 25 and indiceMasaCorporal <= 29.9):
+#         print("Sobrepeso: " + str(indiceMasaCorporal))
+# else:
+#         print("Obesidad: " + str(indiceMasaCorporal))
