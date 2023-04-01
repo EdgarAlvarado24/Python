24f708167e327a8b581c56c6551c497c157e31cf
Edgar Alvarado <edgar.alvarado.16@gmail.com>
2023-03-31 22:42:02 -0400

    mensaje de salida modificado

--- tema7/mi_paquete/fecha.py
+++ tema7/mi_paquete/fecha.py
@@ -2,9 +2,10 @@ import datetime
 
 hora_actual = datetime.datetime.now()
 hora = hora_actual.hour
+minutos = hora_actual.minute
 hora_formateada = hora_actual.strftime('%-I:%M%p')
 
-if hora > 7:
+if hora > 19:
     print(f"Son las {hora_formateada} es hora de ir a casa")
 else:
-    print(f"Son las {hora_formateada} Hay que seguir trabajando")
+    print(f"Quedan {19-hora}H:{minutos}m para salir del trabajo")
