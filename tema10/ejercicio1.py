5953dd2aa7397c21e136ec6ec146f0eddbbaf09f
Edgar Alvarado <edgar.alvarado.16@gmail.com>
2023-05-18 11:15:56 -0400

    Ejercicio 1

--- tema10/ejercicio1.py
+++ tema10/ejercicio1.py
@@ -1,22 +1,24 @@
 from tkinter import *
-from tkinter import ttk
 
+def seleccionar():
+    monitor.config(text="{}".format(opcion.get()))
 def reset():
-    select.set(None)
+    opcion.set(None)
+    monitor.config(text="")
 
 root = Tk()
-root.title("Ejercicio 1")
+root.title("Medio de Transporte")
+opcion = StringVar()
+opcion.set(None)
 
-mainframe = ttk.Frame(root, padding="5 5 10 10")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+Radiobutton(root, text="Moto", variable=opcion, value='Moto', command=seleccionar).pack(anchor=W)
+Radiobutton(root, text="Carro",  variable=opcion, value='Carro', command=seleccionar).pack(anchor=W)
+Radiobutton(root, text="Barco",  variable=opcion, value='Barco', command=seleccionar).pack(anchor=W)
+Radiobutton(root, text="Avion",  variable=opcion, value='Avion', command=seleccionar).pack(anchor=W)
 
-select = StringVar()
-rButton1 = ttk.Radiobutton(mainframe, text="Opción 1", value=1, variable=select).grid(column=0, row=0, sticky=W)
-rButton2 = ttk.Radiobutton(mainframe, text="Opción 2", value=2, variable=select).grid(column=0, row=1, sticky=W)
-rButton3 = ttk.Radiobutton(mainframe, text="Opción 3", value=3, variable=select).grid(column=0, row=2, sticky=W)
+monitor = Label(root)
+monitor.pack()
 
-ttk.Button(mainframe, text="Reiniciar", command=reset).grid(column=1, row=1, sticky=W)
+Button(root, text="Reiniciar", command=reset).pack()
 
 root.mainloop()
